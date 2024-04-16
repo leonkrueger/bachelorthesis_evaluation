17b527263db2ace274027cd6239b09d398bacca8
Leon Krüger <leonkrueger02@gmail.com>
2024-04-16 09:51:23 +0200

    Evaluation calculation updated

--- evaluate_results.py
+++ evaluate_results.py
@@ -10,17 +10,73 @@ from util.adjustments import EXPERIMENTS
 folder = "bird"
 
 
+def _is_row_in_table(
+    gold_standard_row: list[str], result_table: list[list[str]]
+) -> bool:
+    return any(
+        # Is there a result_row that is a superset of the gold_standard_row?
+        [
+            all(
+                # Is every entry in this result_row?
+                [
+                    gold_standard_entry in result_row
+                    for gold_standard_entry in gold_standard_row
+                ]
+            )
+            for result_row in result_table
+        ]
+    )
+
+
+def _calculate_accuracy_for_pair_of_tables(
+    gold_standard_table: list[list[str]], result_table: list[list[str]]
+) -> int:
+    return len(
+        [
+            gold_standard_row
+            for gold_standard_row in gold_standard_table
+            if _is_row_in_table(gold_standard_row, result_table)
+        ]
+    )
+
+
+def _calculate_accuracy_for_one_table(
+    gold_standard_table: list[list[str]], result: dict[str, list[list[str]]]
+) -> int:
+    return max(
+        [
+            _calculate_accuracy_for_pair_of_tables(gold_standard_table, result_table)
+            for result_table in result.values()
+        ]
+    )
+
+
 def calculate_accuracy(
     results: dict[str, list[list[str]]],
     gold_standard: dict[str, list[list[str]]],
     primary_keys: dict[str, list[str]],
 ) -> float:
+    """Calculates the number of data points in the gold standard that ended up together in the resulting database.
+    It does this for each table in the gold standard and returns the percentage of data points.
+    Example:
+    Gold standard:
+    table1: [[1, A], [2, B]]
+    table2: [[Apple], [Banana], [Peach]]
+    Result:
+    tableA: [[1, A], [NULL, Peach], [2, B], [NULL, Apple]]
+    tableB: [[Banana]]
+    Accuracy:
+    (2 + 2) / (2 + 3) = 0.8"""
     correct_inserts = 0
     total_inserts = 0
     for query, gold_standard_table in gold_standard.items():
+        total_inserts += len(gold_standard_table)
+        correct_inserts += _calculate_accuracy_for_one_table(
+            gold_standard_table, results
+        )
+
         # If the table does not exist in the result, continue with the next.
         if not query in results.keys():
-            total_inserts += len(gold_standard_table)
             continue
 
         for gold_standard_row in gold_standard_table:
@@ -31,7 +87,6 @@ def calculate_accuracy(
                 ]
             ):
                 correct_inserts += 1
-            total_inserts += 1
 
     return correct_inserts / float(total_inserts)
 
